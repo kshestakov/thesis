c5dd0233dd77c02788cca282d08f8306f0b1cb3b
Leonid Mosenkov <lmosenkov@gmail.com>
2016-05-12 18:59:09 +0400

    Structure reformatted

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
-	entry: './src/main.js',
-	output: {
-		path: './build/',
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-  			loader: 'babel',
-				query: {
-					presets: ['es2015', 'react', 'react-hmre'],
-          plugins: ['transform-runtime']
-				}
-			}
-		]
-	},
-  devtool: "source-map"
+    entry: './src/main.js',
+    output: {
+        path: './build',
+		publicPath: './build',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: path.join(__dirname, 'src'),
+                query: {
+                  presets: ['es2015', 'react']
+                },
+            }
+        ]
+    },
+    plugins: [
+        // Avoid publishing files when compilation fails
+        new webpack.NoErrorsPlugin()
+    ],
+    stats: {
+        // Nice colored output
+        colors: true
+    },
+    // Create Sourcemaps for the bundle
+    devtool: 'source-map',
 };
